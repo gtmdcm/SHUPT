7d35a17d51a84c0fdf24989b81a6917f75203629
zhoudian64 <1104127978@qq.com>
2018-03-27 22:24:08 +0800

    api 示例

---
+++ resources/assets/js/components/hello.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <h2>{{msg}}</h2>
+        <state></state>
+    </div>
+</template>
+
+<script>
+    import state from './state'
+    export default {
+        data() {
+            return {
+                msg: 'This is a Laravel with Vue and Element Demo.'
+            }
+        },
+        components: {
+            state
+        }
+    }
+</script>
+
+<style>
+    .hello {
+        font-size: 2em;
+        color: green;
+    }
+</style>
