a4c2378976d87dd1d666b18e0ce3b8e37e1cf874
zhoudian64 <1104127978@qq.com>
2018-03-28 16:03:38 +0800

    引用vue示例

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,22 +1,28 @@
 <!doctype html>
-<html lang="en">
+<html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="UTF-8">
     <title>SHUPT - @yield('title')</title>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    @yield('content')
-    <a href="{{route('mainpage')}}">MainPage </a>
-    <a href="{{route('upload')}}">Upload </a>
-    <a href="{{route('torrent')}}">Torrent </a>
-    <a href="{{route('viewrequests')}}">ViewRequests </a>
-    <a href="{{route('forums')}}">Forums</a>
-    <br/>
-    @auth
-        <a href="{{ url('/home') }}">Home</a>
+    <div id="app">
+        <p-nav></p-nav>
+        @yield('content')
+        <a href="{{route('mainpage')}}">MainPage </a>
+        <a href="{{route('upload')}}">Upload </a>
+        <a href="{{route('torrent')}}">Torrent </a>
+        <a href="{{route('viewrequests')}}">ViewRequests </a>
+        <a href="{{route('forums')}}">Forums</a>
+        <br/>
+        @auth
+            <a href="{{ url('/home') }}">Home</a>
         @else
             <a href="{{ route('login') }}">Login</a>
             <a href="{{ route('register') }}">Register</a>
-            @endauth
+        @endauth
+    </div>
 </body>
 </html>
