f778b96bc22b8e72b4347df6de6ccb18b7425626
kyloris0660 <1489821378@qq.com>
2018-03-29 21:49:29 +0800

    fixed profile vue bugs and add new bugs

---
+++ resources/assets/js/components/date.vue
@@ -0,0 +1,15 @@
+<template>
+    <Row>
+        <Col span="12">
+        <DatePicker type="date" placeholder="Select date" style="width: 200px"></DatePicker>
+        </Col>
+        <Col span="12">
+        <DatePicker type="daterange" placement="bottom-end" placeholder="Select date" style="width: 200px"></DatePicker>
+        </Col>
+    </Row>
+</template>
+<script>
+    export default {
+
+    }
+</script>
