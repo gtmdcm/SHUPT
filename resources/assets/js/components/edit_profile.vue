8d061df31657da8ae968da8e7c262611c14933f0
kyloris0660 <1489821378@qq.com>
2018-03-30 21:33:28 +0800

    fix version bug

--- resources/assets/js/components/edit_profile.vue
+++ resources/assets/js/components/edit_profile.vue
@@ -1,61 +1,74 @@
 <template>
-    <form action="test" method="post">
-        昵称:<br>
-        <input type="string" name="nick_name">
-        <br>
-        性别:<br>
-        <input type="radio" name="gender" value="male" checked>男
-        <input type="radio" name="gender" value="female">女
-        <br>
-        生日:
-        <select name="month" onChange="changeDate(this.options[selectedIndex].value);">
-            <option value="na">Month</option>
-            <option value="1">January</option>
-            <option value="2">February</option>
-            <option value="3">March</option>
-            <option value="4">April</option>
-            <option value="5">May</option>
-            <option value="6">June</option>
-            <option value="7">July</option>
-            <option value="8">August</option>
-            <option value="9">September</option>
-            <option value="10">October</option>
-            <option value="11">November</option>
-            <option value="12">December</option>
-        </select>
-        <select name="day" id="day">
-            <option value="na">日</option>
-        </select>
-        <select name="year" id="year">
-            <option value="na">年</option>
-        </select>
-        <br/>
-        地区:<br>
-        <input type="string" name="city">
-        <br>
-        邮箱:<br>
-        <input type="string" name="email">
-        <br>
-        QQ:<br>
-        <input type="string" name="qq">
-        <br>
-        电话:<br>
-        <input type="string" name="tel">
-        <br>
-        自我介绍:<br>
-        <input type="string" name="nick_name"
-               size="200">
-        <button type="submit">确定</button>
-        <br>
-    </form>
+    <Form :model="formItem" :label-width="80">
+        <FormItem label="Input">
+            <Input v-model="formItem.input" placeholder="Enter something..."></Input>
+        </FormItem>
+        <FormItem label="Select">
+            <Select v-model="formItem.select">
+                <Option value="beijing">New York</Option>
+                <Option value="shanghai">London</Option>
+                <Option value="shenzhen">Sydney</Option>
+            </Select>
+        </FormItem>
+        <FormItem label="DatePicker">
+            <Row>
+                <Col span="11">
+                <DatePicker type="date" placeholder="Select date" v-model="formItem.date"></DatePicker>
+                </Col>
+                <Col span="2" style="text-align: center">-</Col>
+                <Col span="11">
+                <TimePicker type="time" placeholder="Select time" v-model="formItem.time"></TimePicker>
+                </Col>
+            </Row>
+        </FormItem>
+        <FormItem label="Radio">
+            <RadioGroup v-model="formItem.radio">
+                <Radio label="male">Male</Radio>
+                <Radio label="female">Female</Radio>
+            </RadioGroup>
+        </FormItem>
+        <FormItem label="Checkbox">
+            <CheckboxGroup v-model="formItem.checkbox">
+                <Checkbox label="Eat"></Checkbox>
+                <Checkbox label="Sleep"></Checkbox>
+                <Checkbox label="Run"></Checkbox>
+                <Checkbox label="Movie"></Checkbox>
+            </CheckboxGroup>
+        </FormItem>
+        <FormItem label="Switch">
+            <i-switch v-model="formItem.switch" size="large">
+                <span slot="open">On</span>
+                <span slot="close">Off</span>
+            </i-switch>
+        </FormItem>
+        <FormItem label="Slider">
+            <Slider v-model="formItem.slider" range></Slider>
+        </FormItem>
+        <FormItem label="Text">
+            <Input v-model="formItem.textarea" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="Enter something..."></Input>
+        </FormItem>
+        <FormItem>
+            <Button type="primary">Submit</Button>
+            <Button type="ghost" style="margin-left: 8px">Cancel</Button>
+        </FormItem>
+    </Form>
 </template>
-
 <script>
     export default {
-        name: "edit_profile"
+        data () {
+            return {
+                formItem: {
+                    input: '',
+                    select: '',
+                    radio: 'male',
+                    checkbox: [],
+                    switch: true,
+                    date: '',
+                    time: '',
+                    slider: [20, 50],
+                    textarea: ''
+                }
+            }
+        }
     }
 </script>
-
-<style scoped>
-
-</style>
