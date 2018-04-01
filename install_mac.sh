b531c565bbe8af311babdd2311beba570e6bd2d5
zhoudian64 <1104127978@qq.com>
2018-04-01 15:19:41 +0800

    安装脚本修正

--- install_mac.sh
+++ install_mac.sh
@@ -3,30 +3,36 @@ npm install
 cp .env.example .env
 php artisan key:generate >> key
 str=$(cat key | cut -b 18-68)
-sed -i "" "s/examplekey/$str/" .env
-echo "输入要使用的数据库(默认mysql)"
-dbc=mysql
-read dbc
+sed -e '4d' ./.env
+echo APP_KEY=$str >> ./.env
+read -p "输入要使用的数据库(默认mysql)" dbc
+if [ -z "${dbc}" ];then
+    dbc=mysql
+fi
 sed -i "" "s/examplesql/$dbc/" .env
-echo "输入数据库ip地址(默认127.0.0.1)"
-dbh=127.0.0.1
-read dbh
-sed -i "" "s/examplehost/$dbc/" .env
-echo "输入数据库开放端口(默认3306)"
-dbp=3306
-read dbp
+read -p "输入数据库ip地址(默认127.0.0.1)" dbh
+if [ -z "${dbh}" ];then
+    dbh='127.0.0.1'
+fi
+sed -i "" "s/examplehost/$dbh/" .env
+read -p "输入数据库开放端口(默认3306)" dbp
+if [ -z "${dbp}" ];then
+    dbp=3306
+fi
 sed -i "" "s/exampleport/$dbp/" .env
-echo "输入要使用的数据库(默认shupt)"
-dbd=shupt
-read dbd
+read -p "输入要使用的数据库(默认shupt)" dbd
+if [ -z "${dbd}" ];then
+    dbd=shupt
+fi
 sed -i "" "s/exampledatabase/$dbd/" .env
-echo "输入数据库用户名(默认root)"
-dbu=root
-read dbu
+read -p "输入数据库用户名(默认root)" dbu
+if [ -z "${dbu}" ];then
+    dbu=root
+fi
 sed -i "" "s/exampleusername/$dbu/" .env
-echo "输入用户密码"
-read dbp
-sed -i "" "s/examplepassword/$dbp/" .env
+echo
+read -p "输入用户密码" dbp
+sed -e '14d' ./.env
 php artisan migrate:fresh
 rm key
 npm run dev
