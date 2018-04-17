12da572152ec8bd9662f6916b1f34355a6666a76
JackySong <1055335350@qq.com>
2018-04-17 11:17:44 +0800

    Find a BEncode Packege (still have some problems)

--- config/app.php
+++ config/app.php
@@ -151,6 +151,8 @@ return [
          * Package Service Providers...
          */
 
+        //Bhutanio\BEncode\ServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -208,6 +210,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        //'BEncode' => Bhutanio\BEncode\Facade::class,
 
     ],
 
