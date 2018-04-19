733559058274dfb4fdf57090485ebc536ad9940e
JackySong <1055335350@qq.com>
2018-04-19 10:24:37 +0800

    User Profile API (v3)

---
+++ app/Http/Controllers/UserInfoController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use App\User;
+
+class UserInfoController extends Controller
+{
+    public function getUserInfo(){
+        $id = Auth::id();
+        $result=DB::select("select birthday,gender,school,signature from users where id = $id");
+        return $result;
+    }
+}
